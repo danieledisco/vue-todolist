d8e8ebca55df968b2911f3f7c5d0c741072b6d8f
danieledisco <daniele.disco.1965@gmail.com>
2024-02-02 10:35:27 +0100

    Milestore 2

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà d
 ## MILESTONE 2
 Visualizzare a fianco ad ogni item una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
-
 # NOTA 
 In index.html è stato aggiunto il link a font-awesome per usare l'icona della croce nel cerchio
+
+## MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": 
+cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
+che quindi viene aggiunto alla lista dei todo esistenti
+
+## Bonus 1:
+oltre al click sul pulsante, intercettare anche il tasto 
+ENTER per aggiungere il todo alla lista
+
+## Bonus 2:
+cliccando sul testo dell'item, invertire il valore della proprietà 
+done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,69 @@
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      toDoList:
-        [
-          {
-            text: 'Acquistare Pane',
-            done: true
-          }
-          ,
-          {
-            text: 'Acquistare Latte',
-            done: false
-          }
-          ,
-          {
-            text: 'Comprare il Giornale',
-            done: false
-          }
-        ]
-    }
-  },
-  methods: {
-    removeTask(index) {
-      console.log('Remove task', index);
-      this.toDoList.splice(index, 1);
+export default
+  {
+    name: 'App',
+    data() {
+      return {
+        toDoList:
+          [
+            {
+              text: 'Acquistare Pane',
+              done: true
+            }
+            ,
+            {
+              text: 'Acquistare Latte',
+              done: false
+            }
+            ,
+            {
+              text: 'Comprare il Giornale',
+              done: false
+            }
+          ],
+        newTask: 'Lavatrice',
+        error: false
+      }
+    },
+    methods: {
+      removeTask(index) {
+        console.log('Remove task', index);
+        this.toDoList.splice(index, 1);
+      },
+      addTask() {
+        console.log("Add task");
+        const obj = { text: this.newTask, done: false };
+        const checkUsername = tobj => tobj.text === this.newTask;
+        if (this.toDoList.some(checkUsername)) {
+          this.error = 'Il compito già presente nella lista';
+        }
+        else if (this.newTask.length > 5) {
+
+          this.toDoList.unshift(obj);
+          this.newTask = '';
+          this.error = false;
+        }
+        else {
+          this.error = 'Il compito deve essere lungo almeno 5 caratteri'
+        }
+      }
     }
   }
-}
 </script>
 
 <template>
+  <h1>Lista ToDo</h1>
+  <span calss="inputgrup">
+    <input type="text" v-model="newTask">
+    <button @click="addTask">Aggiungi compito</button>
+  </span>
+  <br>
+  <span>
+    <div class="msgError" v-if="error">
+      {{ error }}
+    </div>
+  </span>
   <ul>
-    <h1>Lista ToDo</h1>
     <li v-for="(task, index) in toDoList">
       <span :style="{ textDecoration: task.done ? 'line-through' : '' }">
         {{ task.text }}
@@ -53,4 +83,12 @@ i {
   font-size: 1.5rem;
   padding: 0.5rem;
 }
+
+.msgError {
+  color: red;
+  margin: 2rem;
+  height: 1.5rem;
+  font-size: 1.5rem;
+}
 </style>
+
